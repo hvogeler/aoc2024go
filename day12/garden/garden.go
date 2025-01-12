3694fa2565d7aba405107f642c2b96c459ff0a44
Heiko Vogeler <hvogeler@abinitio.com>
2025-01-12 19:07:11 +0100

    in region walk

--- day12/garden/garden.go
+++ day12/garden/garden.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Garden struct {
-	area [][]Plot
+	area       [][]Plot
 	regionsMap map[PlantType][]Plot
 	dimensions Dimensions
 }
@@ -44,13 +44,83 @@ func GardenFromStr(data string) Garden {
 	}
 	garden.regionsMap = make(map[PlantType][]Plot)
 	garden.dimensions = Dimensions{rowno, len(garden.area[0])}
+	garden.checkNeighbors()
 	return *garden
 }
 
+func (garden *Garden) checkNeighbors() {
+	for row := 0; row < garden.dimensions.rows; row++ {
+		for col := 0; col < garden.dimensions.cols; col++ {
+			plot := &garden.area[row][col]
+
+			plotRow := plot.location.row
+			plotCol := plot.location.col
+			// check above
+			if plotRow > 0 && garden.area[plotRow-1][plotCol].plantType == plot.plantType {
+				plot.neighbors[above] = &garden.area[plotRow-1][plotCol]
+			}
+			// check below
+			if plotRow < garden.dimensions.rows-1 && garden.area[plotRow+1][plotCol].plantType == plot.plantType {
+				plot.neighbors[below] = &garden.area[plotRow+1][plotCol]
+			}
+			// check right
+			if plotCol < garden.dimensions.cols-1 && garden.area[plotRow][plotCol+1].plantType == plot.plantType {
+				plot.neighbors[right] = &garden.area[plotRow][plotCol+1]
+			}
+			// check left
+			if plotCol > 0 && garden.area[plotRow][plotCol-1].plantType == plot.plantType {
+				plot.neighbors[left] = &garden.area[plotRow][plotCol-1]
+			}
+		}
+	}
+}
+
+// func (garden *Garden) findRegions() {
+// 	for row := 0; row < garden.dimensions.rows; row++ {
+// 		for col := 0; col < garden.dimensions.cols; col++ {
+// 			plot := garden.area[row][col]
+// 			if _, regionExists := garden.regionsMap[plot.plantType]; !regionExists {
+// 				garden.exploreRegion(plot)
+// 			}
+// 		}
+// 	}
+// }
+
+// func (garden Garden) exploreRegion(plot Plot) {
+
+// }
+
+type Region []Plot
+
+func (region Region) contains(plot *Plot) bool {
+	for _, existingPlot := range region {
+		if existingPlot.Equals(*plot) {
+			return true
+		}
+	}
+	return false
+}
+
+func (plot Plot) WalkPlot(region *Region) {
+	*region = append(*region, plot)
+	for direction := above; direction <= left; direction++ {
+		if plot.neighbors[direction] != nil && !region.contains(plot.neighbors[direction]) {
+			plot.neighbors[direction].WalkPlot(region)
+		}
+	}
+}
+
+const (
+	above = iota
+	right
+	below
+	left
+)
+
 type Plot struct {
 	plantType PlantType
-	location Location
-	fences []Fence
+	location  Location
+	neighbors [4]*Plot
 }
 
 func (a Plot) Equals(b Plot) bool {
@@ -62,25 +132,30 @@ func (a Plot) Equals(b Plot) bool {
 }
 
 type PlantType rune
+
 func (plantType PlantType) String() string {
 	return string(plantType)
 }
 
-type Fence struct {
-	fenceType FenceType
-}
+// type Fence struct {
+// 	fenceType FenceType
+// }
 
-type FenceType int
-const (
-	top FenceType = iota
-	bottom
-	left
-	right
-)
+// type FenceType int
+// const (
+// 	top FenceType = iota
+// 	bottom
+// 	left
+// 	right
+// )
 
 type Location struct {
 	row int
 	col int
+}
+
+func (loc Location) String() string {
+	return fmt.Sprintf("(%d, %d)", loc.row, loc.col)
 }
 
 type Dimensions struct {
--- day12/garden/garten_test.go
+++ day12/garden/garten_test.go
@@ -17,7 +17,24 @@ MIIISIJEEE
 MMMISSJEEE`
 
 func Test_FromStr(t *testing.T) {
-	t.Run("DoNGenMap", func(t *testing.T) {
+	t.Run("WalkPlots", func(t *testing.T) {
+		garden := GardenFromStr(example3)
+		region := new(Region)
+		garden.area[0][0].WalkPlot(region)
+		if len(*region) != 12 {
+			t.Errorf("Expected region 'R' to contain 12 plots, but got %d", len(*region))
+		}
+		garden.area[2][3].WalkPlot(region)
+		if len(*region) != 12 {
+			t.Errorf("Expected region 'R' to contain 12 plots, but got %d", len(*region))
+		}
+		// garden.area[0][0].WalkPlot(region)
+		// if len(*region) != 12 {
+		// 	t.Errorf("Expected region 'R' to contain 12 plots, but got %d", len(*region))
+		// }
+	})
+
+	t.Run("FromStr", func(t *testing.T) {
 		garden := GardenFromStr(example3)
 		if garden.dimensions.rows != 10 {
 			t.Errorf("Expected 10 rows, got %d", garden.dimensions.rows)
@@ -25,9 +42,41 @@ func Test_FromStr(t *testing.T) {
 		if garden.dimensions.cols != 10 {
 			t.Errorf("Expected 10 columns, got %d", garden.dimensions.cols)
 		}
-		if !garden.area[2][2].Equals(Plot{PlantType('R'), Location{2, 2}, []Fence{}})  {
-			t.Errorf("Expected %v got %v", Plot{PlantType('R'), Location{2, 2}, []Fence{}}, garden.area[2][2])
+		plot := garden.area[2][2]
+		if plot.plantType != PlantType('R') {
+			t.Errorf("Expected planttype 'R', got %s", plot.plantType)
 		}
+		if plot.neighbors[above] == nil || plot.neighbors[above].plantType != PlantType('R') {
+			t.Errorf("Expected neighbor above of planttype 'R'")
+		}
+		if plot.neighbors != [4]*Plot{&garden.area[1][2], &garden.area[2][3], &garden.area[3][2], nil} {
+			t.Errorf("Expected other neighbors")
+		}
+
+		plot = garden.area[9][0]
+		if plot.plantType != PlantType('M') {
+			t.Errorf("Expected planttype 'M', got %s", plot.plantType)
+		}
+		if plot.neighbors != [4]*Plot{&garden.area[8][0], &garden.area[9][1], nil, nil} {
+			t.Errorf("Expected other neighbors")
+		}
+
+		plot = garden.area[4][9]
+		if plot.plantType != PlantType('E') {
+			t.Errorf("Expected planttype 'E', got %s", plot.plantType)
+		}
+		if plot.neighbors != [4]*Plot{nil, nil, &garden.area[5][9], nil} {
+			t.Errorf("Expected other neighbors")
+		}
+
+		plot = garden.area[0][1]
+		if plot.plantType != PlantType('R') {
+			t.Errorf("Expected planttype 'R', got %s", plot.plantType)
+		}
+		if plot.neighbors[right].location != (Location{0, 2}) {
+			t.Errorf("Expected neighbor location (0, 2), got %s", plot.neighbors[right].location)
+		}
+
 		fmt.Println(garden)
 	})
 
